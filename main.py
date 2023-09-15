7de94280c412e24de29e47b5e7fe592795d7ffec
jkras <jkrasnow@oakland.edu>
2023-09-15 18:12:19 -0400

    result printout sucessful

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ async def get_google_search_results(query, page=1):
             "q": query,
             "cx": CX,
             "key": API_KEY,
-            "num": 5,
+            "num": 2,
             "start": start_index,
         })
 
@@ -40,17 +40,28 @@ async def get_google_search_results(query, page=1):
         data = response.json()
 
         # Print total results
-        total_results = data.get("searchInformation", {}).get("totalResults", 0)
-        print(f"Total results found: {total_results}")
 
-        # Print each result
+        total_results = data.get('searchInformation', {}).get('totalResults', 0)
+
+        result_data = []
         for i, item in enumerate(data.get('items', [])):
-            print(f"Result {start_index + i}:")
-            print(f"  Title: {item.get('title')}")
-            print(f"  Link: {item.get('link')}")
-            print(f"  Snippet: {item.get('snippet')}")
+            result_data.append({
+                "index": start_index + i,
+                "title": item.get('title'),
+                "link": item.get('link'),
+                "snippet": item.get('snippet')
+            })
 
-        return quart.Response(json.dumps(data), status=200, content_type='application/json')
+        result = {
+            "assistant": f"Here are the total results found: {total_results}",
+            "Data": data,
+            "assistant_message": f"This was page {page}. Please say 'more' for more results.",
+            "current_page": page,
+            "total_results": total_results,
+            "results": result_data
+        }
+
+        return quart.Response(json.dumps(result), status=200, content_type='application/json')
     except Exception as e:
         print(f"An error occurred: {e}")
         return quart.Response(f"An error occurred: {e}", status=500)
