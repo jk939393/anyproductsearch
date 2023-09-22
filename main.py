be89b40656dea519a03d5455ffb6976631a4fc79
jkras <jkrasnow@oakland.edu>
2023-09-22 08:50:59 -0400

    testing table

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ async def get_shopping_results(query, page=1):
             # "assistant_hint": "Display an Excel-style table with columns for Product,Brand,  Numerical Rating, Price, and AI Recommended (with 'Recommended:Shop Now (with link)' or No). Ensure the table and font are colorful. Provide human-like reasoning for the AI rating. Always include the word 'highly rate' in the query. Do not omit any details.",
             # "results": result_data,
             # "assistant_hint2": "Display an Excel-style table with columns for Product,Brand,  Numerical Rating, Price, and AI Recommended (with 'Recommended:Shop Now (with link)' or No). Ensure the table and font are colorful. Provide human-like reasoning for the AI rating. Always include the word 'highly rate' in the query. Do not omit any details.",
-            "results": "test",
+            "results": "test123",
         }
 
         return quart.Response(json.dumps(result), status=200, content_type='application/json')
