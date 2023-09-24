ccd253e416a153dd629d72b1bc0651fd6256aa17
jkras <jkrasnow@oakland.edu>
2023-09-24 08:32:56 -0400

    proxy test

--- scrape1.py
+++ scrape1.py
@@ -2,12 +2,11 @@ from bs4 import BeautifulSoup
 import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.proxy import Proxy, ProxyType
 import os
 import pyshorteners
 
-
 logging.basicConfig(level=logging.DEBUG)
-
 
 class Scraper:
     def __init__(self):
@@ -20,13 +19,57 @@ class Scraper:
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
 
+        # Set up proxy
+        self.proxies = [
+            "http://34.23.45.223:80",
+            "socks5://70.35.199.99:52344",
+            "socks4://50.7.67.212:63812",
+            "http://123.205.68.113:8193",
+            "http://165.16.60.245:8080",
+            "http://38.156.238.94:999",
+            "http://83.97.20.192:8080",
+            "http://136.243.92.30:26541",
+            "http://185.73.202.125:3128",
+            "http://172.111.10.191:3128",
+            "http://36.37.81.135:8080",
+            "socks4://64.90.51.183:44074",
+            "http://202.57.30.66:80",
+            "http://139.162.99.100:8080"
+        ]
+        selected_proxy = self.select_proxy()  # Select a proxy from the list
+        if selected_proxy:
+            chrome_options = self.set_proxy(chrome_options, selected_proxy)
+
         chromedriver_path = os.environ.get("CHROMEDRIVER_PATH")
         if chromedriver_path:
-            self.driver = webdriver.Chrome(options=chrome_options)
+            self.driver = webdriver.Chrome(chromedriver_path, options=chrome_options)
         else:
             self.driver = webdriver.Chrome(options=chrome_options)
 
         self.shortener = pyshorteners.Shortener()
+
+    def select_proxy(self):
+        # For simplicity, we're just picking a random proxy. You can implement more complex logic.
+        import random
+        return random.choice(self.proxies)
+
+    def set_proxy(self, chrome_options, proxy):
+        proxy_obj = Proxy()
+        proxy_obj.proxy_type = ProxyType.MANUAL
+        if "http" in proxy:
+            proxy_obj.http_proxy = proxy
+            proxy_obj.ssl_proxy = proxy
+        elif "socks4" in proxy:
+            proxy_obj.socks4_proxy = proxy
+        elif "socks5" in proxy:
+            proxy_obj.socks5_proxy = proxy
+
+        capabilities = webdriver.DesiredCapabilities.CHROME
+        proxy_obj.add_to_capabilities(capabilities)
+
+        chrome_options.merge(capabilities)
+
+        return chrome_options
 
     def scrape_content(self, urls):
         results = []
